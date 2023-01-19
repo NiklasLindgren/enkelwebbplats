f0c9b46a7abed4f0ea0124c4fa4bb0a73614ad8e
NiklasLindgren <niklas.lindgren@student.tabyenskilda.se>
2023-01-19 12:31:01 +0100

    j
    v

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+/* göm/ visa rubrik*/
+
+function visarubrik(){
+document.getElementById('h2').style.visibility = 'visible';
+}
+function gömrubrik(){
+    document.getElementById('h2').style.visibility = 'hidden'
+}
+
+/*byt bild/logga*/
+function bytillbild(){
+    document.getElementById('logga').src='img/img/Season_2_-_Jesse.webp';
+}
+function byttilllogga(){
+    document.getElementById('logga').src= 'img/Walter_White_S5B.png.webp';
+}
+  
